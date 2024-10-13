95e054e615504325395d122e5f0a1abe059c24b4
vipsbpig <vipsbpig@gmail.com>
2024-10-13 21:59:17 +0800

    update code

--- Scripts/input_behaviour.gd
+++ Scripts/input_behaviour.gd
@@ -2,7 +2,8 @@ extends Node
 
 @export var rigidBody : RigidBody3D
 
-@export var moveSpeed : float = 5
+@export var maxSpeed : float = 12
+@export var rotateSpeed : float = 5
 @export var jumpForce : float = 1
 @export var rotaionDampForce : float = 10
 
@@ -18,7 +19,7 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("move_right"):
 		moveDirection = -1
 	
-	if moveDirection != 0: velocity.z = moveSpeed * moveDirection * delta
+	if moveDirection != 0: velocity.z = rotateSpeed * moveDirection * delta
 	else: velocity.z = lerp(velocity.z, 0.0, rotaionDampForce * fixedDeltaTime)
 
 	rigidBody.angular_velocity = velocity
@@ -26,3 +27,13 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		var local_x_direction = rigidBody.transform.basis.x
 		rigidBody.apply_force(local_x_direction * jumpForce)
+		
+	# 计算速度的大小
+	var current_velocity = rigidBody.linear_velocity
+	var speed = current_velocity.length()
+	# 检查是否超过最大速度
+	if speed > maxSpeed:
+		# 计算限制后的速度向量
+		var limited_velocity = current_velocity.normalized() * maxSpeed
+		# 设置刚体的线性速度为限制后的速度
+		rigidBody.linear_velocity = limited_velocity
