7b058925d0b050c9534a0c801a7dc0895bf0700f
dan <i@dan.games>
2024-10-13 22:24:10 +0800

    add player2 & input

--- Scripts/input_behaviour.gd
+++ Scripts/input_behaviour.gd
@@ -7,6 +7,7 @@ extends Node
 @export var rotateTime : float = 0.05
 @export var jumpForce : float = 1
 @export var rotaionDampForce : float = 10
+@export var player_index: int = 0
 
 var fixedDeltaTime : float = 0
 var holdTime = 0
@@ -16,10 +17,16 @@ func _physics_process(delta: float) -> void:
 	
 	var velocity : Vector3 = rigidBody.angular_velocity
 	var rotateDirection : int = 0;
-	if Input.is_action_pressed("move_left"):
-		rotateDirection = 1
-	elif Input.is_action_pressed("move_right"):
-		rotateDirection = -1
+	if player_index == 0:
+		if Input.is_action_pressed("move_left"):
+			rotateDirection = 1
+		elif Input.is_action_pressed("move_right"):
+			rotateDirection = -1
+	else:
+		if Input.is_action_pressed("move_left2"):
+			rotateDirection = 1
+		elif Input.is_action_pressed("move_right2"):
+			rotateDirection = -1
 	
 	if Input.is_action_just_pressed("move_left") || Input.is_action_just_pressed("move_right"):
 		holdTime = rotateTime
@@ -30,10 +37,14 @@ func _physics_process(delta: float) -> void:
 	else: velocity.z = lerp(velocity.z, 0.0, rotaionDampForce * fixedDeltaTime)
 
 	rigidBody.angular_velocity = velocity
-		
-	if Input.is_action_just_pressed("jump"):
-		var local_x_direction = rigidBody.transform.basis.x
-		rigidBody.apply_force(local_x_direction * jumpForce)
+	if player_index == 0:
+		if Input.is_action_just_pressed("jump"):
+			var local_x_direction = rigidBody.transform.basis.x
+			rigidBody.apply_force(local_x_direction * jumpForce)
+	else:
+		if Input.is_action_just_pressed("jump2"):
+			var local_x_direction = rigidBody.transform.basis.x
+			rigidBody.apply_force(local_x_direction * jumpForce)
 		
 	# 计算速度的大小
 	var current_velocity = rigidBody.linear_velocity
