cbcb032d1abd1d9d557826764d861ff339d2d586
vipsbpig <vipsbpig@gmail.com>
2024-10-13 21:40:46 +0800

    update ballon

--- Scripts/input_behaviour.gd
+++ Scripts/input_behaviour.gd
@@ -4,22 +4,25 @@ extends Node
 
 @export var moveSpeed : float = 5
 @export var jumpForce : float = 1
+@export var rotaionDampForce : float = 10
 
 var fixedDeltaTime : float = 0
 
 func _physics_process(delta: float) -> void:
 	fixedDeltaTime = delta	
 	
-	var velocity : Vector3 = rigidBody.linear_velocity
+	var velocity : Vector3 = rigidBody.angular_velocity
 	var moveDirection : int = 0;
 	if Input.is_action_pressed("move_left"):
-		moveDirection = -1
-	elif Input.is_action_pressed("move_right"):
 		moveDirection = 1
+	elif Input.is_action_pressed("move_right"):
+		moveDirection = -1
 	
-	if moveDirection != 0: velocity.x = moveSpeed * moveDirection 
+	if moveDirection != 0: velocity.z = moveSpeed * moveDirection * delta
+	else: velocity.z = lerp(velocity.z, 0.0, rotaionDampForce * fixedDeltaTime)
 
-	rigidBody.linear_velocity = velocity	
+	rigidBody.angular_velocity = velocity	
 		
 	if Input.is_action_just_pressed("jump"):
-		rigidBody.apply_force(Vector3(0, jumpForce, 0))
+		var local_x_direction = rigidBody.transform.basis.x
+		rigidBody.apply_force(local_x_direction * jumpForce)
