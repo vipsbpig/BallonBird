372ab813af96a997f7c7980b84114505d743767b
vipsbpig <vipsbpig@gmail.com>
2024-10-20 15:13:35 +0800

    add follow damp

--- Scripts/camera_controller.gd
+++ Scripts/camera_controller.gd
@@ -8,11 +8,10 @@ func _enter_tree() -> void:
 	Global.CameraContoller = self
 	
 func _ready() -> void:
+	pCam.set_follow_damping(true)
 	pCam.set_auto_follow_distance(true)
 	pCam.set_auto_follow_distance_min(distanc_min_max.x)
 	pCam.set_auto_follow_distance_max(distanc_min_max.y)
-	#pass
-	#pCam.set_auto_follow_distance(4)
 
 func AppendFollow(follow: Node3D)->void:
 	pCam.append_follow_targets(follow)
